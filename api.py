232ebd94ba8622aaea795b1c2822f87660d5bdcf
Travis Beatty <travisby@gmail.com>
2012-11-10 13:09:04 -0500

    Added the Api class, its initializer, and instantiated the request objects

---
+++ api.py
@@ -0,0 +1,41 @@
+"""Api and Utilities"""
+import httplib2
+
+
+class Api():
+    """Api Object"""
+
+    base_url = ''  # Prefixed before every URL.  EX: api.website.url/v2
+    endpoints = None  # Dictionary holding all the options
+    request_handler = None  # httplib2-esque
+
+    # This is how we handle our different types of requests...
+    _method = {
+        'get': 'GET',
+        'post': 'POST',
+        'put': 'PUT',
+        'delete': 'DELETE',
+        'head': 'HEAD'
+    }
+
+    def __init__(self, base_url, username='', password='', oauth_client=None):
+        """Grabs everything we need to connect to a REST API
+            base_url: 'http://google.com'  -- no trailing slashes
+            username = 'travisby@gmail.com'
+            password = 'helloworld'
+            oauth_client = oauth2.client()
+        """
+        self.base_url = base_url
+        self.endpoints = {}
+        if oauth_client:
+            self.request_handler = oauth_client
+        else:
+            self.request_handler = Api._httplib2_init(username, password)
+
+    @staticmethod
+    def _httplib2_init(username, password):
+        """Used to instantiate a regular HTTP request object"""
+        obj = httplib2.Http()
+        if username and password:
+            obj.add_credentials(username, password)
+        return obj
