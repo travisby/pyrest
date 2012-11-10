7a4caa16dd0a771e5d659f88c24dcbfa0b9f2667
Travis Beatty <travisby@gmail.com>
2012-11-10 13:33:08 -0500

    Added copyright to pyrest

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
-"""Small RESTFul API Wrapper"""
+"""
+Small RESTFul API Wrapper
+
+    This file is part of pyrest.
+
+    pyrest is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    pyrest is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pyrest.  If not, see <http://www.gnu.org/licenses/>.
+"""
--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
-"""Used for Distribution of pyrest"""
+"""
+Used for Distribution
+
+    This file is part of pyrest.
+
+    pyrest is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    pyrest is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pyrest.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 from distutils.core import setup
 
